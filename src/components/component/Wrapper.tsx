edd9fc816573375490b298d3cab1143b0df13eef
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-08-11 19:24:56 +0530

    adds: icon pack

--- src/components/component/Wrapper.tsx
+++ src/components/component/Wrapper.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button';
 import clsx from 'clsx';
+import { DownloadIcon, HelpCircle, Share, ShieldQuestion, X } from 'lucide-react';
 
 
 function Wrapper({ children }: { children: React.ReactNode; }) {
@@ -28,13 +29,13 @@ function Wrapper({ children }: { children: React.ReactNode; }) {
     };
     return (
         <>
-            <div className="flex justify-end items-center w-full fixed top-0">
+            <div className={clsx(showInstructions ? "blur-sm" : "","flex justify-end items-center w-full fixed top-0")}>
                 {showInstallPrompt && (
                     <Button onClick={handleInstall} className="m-2">
-                        Install App
+                        <DownloadIcon className='mr-2' /> Install App
                     </Button>
                 )}
-                <Button onClick={() => setShowInstructions(true)} className="m-2">?</Button>
+                <Button onClick={() => setShowInstructions(true)} className="m-2"> <HelpCircle className='mr-2' />  Help</Button>
             </div>
             <div className={clsx(showInstructions ? "blur-sm" : "", "flex min-h-screen flex-col items-center justify-center p-4")}>
                 {children}
@@ -43,18 +44,18 @@ function Wrapper({ children }: { children: React.ReactNode; }) {
                 <div className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center bg-black bg-opacity-50">
                     <div className="instructions-overlay bg-black p-4 rounded-lg border border-solid border-white/50 text-card-foreground shadow-sm m-2">
                         <div className="flex justify-between items-center w-full mb-1">
-                            <h1 className="font-semibold text-gray-200 text-lg">How to use File Share</h1>
-                            <Button onClick={() => setShowInstructions(false)}>X</Button>
+                            <h1 className="font-semibold text-gray-200 text-lg"> <Share className='inline-block mr-2'/> How to use File Share</h1>
+                            <Button onClick={() => setShowInstructions(false)}> <X /> Close </Button>
                         </div>
                         <ul className="list-disc space-y-1 list-inside my-2 text-sm text-gray-500 dark:text-gray-400">
                             <li>Click the "Choose File" button to select one or more files for upload.</li>
                             <li>Click the "Upload" button to upload the selected files to the temporary server.</li>
                             <li>After uploading, you will be provided with a shareable code.</li>
-                            <li>Use the code to retrieve your documents later.</li>
+                            <li>Use the code to retrieve your documents later. </li>
                         </ul>
 
                         <p className=" text-center text-gray-500 text-sm mt-4">
-                            The files will be deleted after 1 to 2 hrs of uploading.
+                            Files will be deleted 24 hrs after upload.
                             <br />
 
                             <a
