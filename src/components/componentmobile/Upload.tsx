6d83581ff19703acc3ad7a2fc173e91bbaf7b7d0
Taarak Gada <taarakgada4151@gmail.com>
2024-10-12 16:51:02 +0530

    Refactor Upload component to reset initial state values

--- src/components/componentmobile/Upload.tsx
+++ src/components/componentmobile/Upload.tsx
@@ -12,12 +12,12 @@ import { UploadCloud } from 'lucide-react';
 
 function Upload() {
     const [loading, setLoading] = useState(false);
-    const [uniqueCode, setUniqueCode] = useState<string>('ig11');
+    const [uniqueCode, setUniqueCode] = useState<string>('');
     const pb = new PocketBase('https://sujal.pockethost.io');
     const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
     const [activeTab, setActiveTab] = useState('files');
     const [textInput, setTextInput] = useState('');
-    const [isUploaded, setIsUploaded] = useState(true);
+    const [isUploaded, setIsUploaded] = useState(false);
 
     const handleTabChange = (newTab: string) => {
         if (newTab !== activeTab) {
